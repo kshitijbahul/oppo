07cb7083daac2dda29a2c773ae4c651749a30a2a
KshitijBahul <chunks.3@gmail.com>
2019-11-28 19:37:02 +0530

    Integrated Euraka client

--- src/main/java/com/kshitij/assignments/oppo/controllers/LoanController.java
+++ src/main/java/com/kshitij/assignments/oppo/controllers/LoanController.java
@@ -8,8 +8,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:3002")
 public class LoanController {
 
     @Autowired
